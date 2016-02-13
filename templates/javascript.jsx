dadb6c16a90f355213ed929442104b09d84aacf5
Chris Kobrzak <chris.kobrzak@argos.co.uk>
2016-02-13 19:13:15 +0000

    Fix syntax issues in JSX template to reconcile Standard

--- templates/javascript.jsx
+++ templates/javascript.jsx
@@ -1,40 +1,36 @@
 import React, { PropTypes } from 'react'
 
 /*
-const ThisClass = ( props ) => (
-  <ThisClass key={ props.foo } />
+const ThisClass = (props) => (
+  <ThisClass key={props.foo} />
 )
 // or:
 */
 
 export default class ThisClass extends React.Component {
 
-  constructor(props) {
+  constructor (props) {
     super(props)
-    //this.bindInstanceMethods( 'methodName1', 'methodName2' )
+    this.bindInstanceMethods('methodName1', 'methodName2')
 
-    //this.state = {}
+    // this.state = {}
   }
 
-  bindInstanceMethods( ...methods ) {
-    methods.forEach(
-      ( method ) => this[ method ] = this[ method ].bind( this )
-    )
+  bindInstanceMethods (...methods) {
+    methods.forEach((method) => { this[method] = this[method].bind(this) })
   }
 
-  /*
   // https://facebook.github.io/react/docs/component-specs.html
-  componentWillMount() { }
-  componentDidMount() { }
-  componentWillReceiveProps( nextProps ) { }
-  shouldComponentUpdate( nextProps, nextState ) { return false }
-  componentWillUpdate( nextProps, nextState ) { }
-  componentDidUpdate( prevProps, prevState ) { }
-  componentWillUnmount() { }
-  */
+  componentWillMount () { }
+  componentDidMount () { }
+  componentWillReceiveProps (nextProps) { }
+  shouldComponentUpdate (nextProps, nextState) { return false }
+  componentWillUpdate (nextProps, nextState) { }
+  componentDidUpdate (prevProps, prevState) { }
+  componentWillUnmount () { }
 
   // Consider hitting m-r in Vim to set bookmark
-  render() {
+  render () {
     return (
       <div>
       </div>
@@ -42,10 +38,11 @@ export default class ThisClass extends React.Component {
   }
 }
 
-/*
 ThisClass.propTypes = {
+  string: PropTypes.string.isRequired,
   initialValue: PropTypes.number,
-  type: PropTypes.oneOf( ['enum1', 'enum2'] )
+  type: PropTypes.oneOf(['enum1', 'enum2'])
 }
-*/
-// ThisClass.defaultProps = { initialValue: 0 }
+ThisClass.defaultProps = {
+  initialValue: 0
+}
